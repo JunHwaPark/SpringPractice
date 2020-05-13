ff2e88e34c2de325fa800e5c210eda2ccfdec384
JunHwaPark <a28165415@gmail.com>
2020-05-13 17:09:25 +0900

    Chapter 03

---
+++ src/main/java/com/junhwa/springboot/service/PostsService.java
@@ -0,0 +1,37 @@
+package com.junhwa.springboot.service;
+
+import com.junhwa.springboot.domain.posts.Posts;
+import com.junhwa.springboot.domain.posts.PostsRepository;
+import com.junhwa.springboot.web.dto.PostsResponseDto;
+import com.junhwa.springboot.web.dto.PostsSaveRequestDto;
+import com.junhwa.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return  new PostsResponseDto(entity);
+    }
+}
